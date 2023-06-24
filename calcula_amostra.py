3d8a2c1f44a993fde4939725742fd1699a247a53
Matheus S Cordaro <cordaromatheus@gmail.com>
2023-06-23 22:24:32 -0300

    Update calcula_amostra.py
    Conversão das chaves de str para int

--- calcula_amostra.py
+++ calcula_amostra.py
@@ -1,4 +1,7 @@
 # solicitar o estado o qual será realizado a pesquisa
+def ln():
+    print('--' * 45)
+
 def menu_regiao():
         
     menu = {
@@ -8,37 +11,40 @@ def menu_regiao():
         4: 'Sul',
         5: 'Sudeste'     
         }   
-        
+
+    ln()        
     # percorrendo as opções do menu
     for chave, valor in menu.items():
         print(f' {chave} : {valor}')    
 
     # iniciando o loop para verificar as interações dos menus subsequentes
     while True:
+        ln()
         opcao = int(input('Selecione em qual REGIÃO a pesquisa está sendo realizada: '))
         regiao_selec = menu.get(opcao)
 
+        
         if regiao_selec:
-            
+            ln()
             if opcao == 1:
                 norte = {
-                            '1': 'AC - Acre',
-                            '2': 'AP - Amapá',
-                            '3': 'AM - Amazonas',
-                            '4': 'RR - Roraima',
-                            '5': 'PA - Pará',
-                            '6': 'RO - Rondônia',
-                            '7': 'TO - Tocantins'
+                            1: 'AC - Acre',
+                            2: 'AP - Amapá',
+                            3: 'AM - Amazonas',
+                            4: 'RR - Roraima',
+                            5: 'PA - Pará',
+                            6: 'RO - Rondônia',
+                            7: 'TO - Tocantins'
                 }
                 
                 
 
                 for chave, valor in norte.items():
                     print(f'{chave} : {valor}')
-
-                opcao2 = input('Digite o estado em que a pesquisa está sendo realizada: ')
+                ln()
+                opcao2 = int(input('Digite o estado em que a pesquisa está sendo realizada: '))
                 estado_selec = norte.get(opcao2)
-
+                ln()
                 if estado_selec:
                     print(f'A pesquisa está sendo realizada na região: {regiao_selec}, no estado de {estado_selec}')
                     break
@@ -47,82 +53,83 @@ def menu_regiao():
             
             if opcao == 2:
                 nordeste = {
-                            '1': 'AL - Alagoas',
-                            '2': 'BA - Bahia',
-                            '3': 'CE - Ceará',
-                            '4': 'MA - Maranhão',
-                            '5': 'PB - Paraíba',
-                            '6': 'PE - Pernambuco',
-                            '7': 'PI - Piauí',
-                            '8': 'RN - Rio Grande do Norte',
-                            '9': 'SE - Sergipe'
+                            1: 'AL - Alagoas',
+                            2: 'BA - Bahia',
+                            3: 'CE - Ceará',
+                            4: 'MA - Maranhão',
+                            5: 'PB - Paraíba',
+                            6: 'PE - Pernambuco',
+                            7: 'PI - Piauí',
+                            8: 'RN - Rio Grande do Norte',
+                            9: 'SE - Sergipe'
                                 }
 
                 for chave, valor in nordeste.items():
                     print(f'{chave} : {valor}')
-
-                opcao3 = input('Digite o estado em que a pesquisa está sendo realizada: ')
+                
+                ln()
+                opcao3 = int(input('Digite o estado em que a pesquisa está sendo realizada: '))
                 estado_selec1 = nordeste.get(opcao3)
-
+                ln()
                 if estado_selec1:
                     print(f'A pesquisa está sendo realizada na região: {regiao_selec}, no estado {estado_selec1}')
                     break
                 else:
                     print('Digite uma opção válida')
-
+            
             if opcao == 3:
                 centro_oeste = {
-                            '1': 'DF - Distrito Federal',
-                            '2': 'GO - Goiás',
-                            '3': 'MT - Mato Grosso',
-                            '4': 'MS - Mato Grosso do Sul'
+                            1: 'DF - Distrito Federal',
+                            2: 'GO - Goiás',
+                            3: 'MT - Mato Grosso',
+                            4: 'MS - Mato Grosso do Sul'
                                     } 
                 
                 for chave, valor in centro_oeste.items():
                     print(f'{chave} : {valor}')
-
-                opcao4 = input('Digite em qual estado a pesquisa está sendo realizada: ')
+                ln()
+                opcao4 = int(input('Digite em qual estado a pesquisa está sendo realizada: '))
                 estado_selec2 = centro_oeste.get(opcao4)
-
+                ln()
                 if estado_selec2:
                     print(f'A pesquisa está sendo realizada na região: {regiao_selec}, no estado {estado_selec2}')
                     break
                 else:
                     print('Digite uma opção válida')
-
+            
             if opcao == 4:
                 sul = {
-                            '1': 'PR - Curitiba',
-                            '2': 'RS - Rio Grande do Sul',
-                            '3': 'SC - Santa Catarina'
+                            1: 'PR - Curitiba',
+                            2: 'RS - Rio Grande do Sul',
+                            3: 'SC - Santa Catarina'
                     }
 
                 for chave, valor in sul.items():
                     print(f'{chave} : {valor}')
-
-                opcao5 = input('Digite em qual estado a pesquisa está sendo realizada: ')
+                ln()
+                opcao5 = int(input('Digite em qual estado a pesquisa está sendo realizada: '))
                 estado_selec3 = sul.get(opcao5)
-
+                ln()
                 if estado_selec3:
                     print(f'A pesquisa está sendo realizada na região: {regiao_selec}, no estado {estado_selec3}')
                     break
                 else:
                     print('Digite uma opção válida')
-
+            
             if opcao == 5:
                 sudeste = {
-                            '1': 'ES - Espírito Santo',
-                            '2': 'MG - Minas Gerais',
-                            '3': 'RJ - Rio de Janeiro',
-                            '4': 'SP - São Paulo'
+                            1: 'ES - Espírito Santo',
+                            2: 'MG - Minas Gerais',
+                            3: 'RJ - Rio de Janeiro',
+                            4: 'SP - São Paulo'
                     }
-                  
+                          
                 for chave, valor in sudeste.items():
                     print(f'{chave} : {valor}')
-
-                opcao6 = input('Digite em qual estado a pesquisa está sendo realizada: ')
+                ln()
+                opcao6 = int(input('Digite em qual estado a pesquisa está sendo realizada: '))
                 estado_selec4 = sudeste.get(opcao6)
-
+                ln()
                 if estado_selec4:
                     print(f'A pesquisa está sendo realizada na região: {regiao_selec}, no estado {estado_selec4}')
                     break
@@ -130,6 +137,7 @@ def menu_regiao():
                     print('Digite uma opção válida')
         else:
             print('Digite uma opção válida')            
+
 
 # solicitar dados para calcular adesão/evasão da pesquisa
 def calculando_dados():
